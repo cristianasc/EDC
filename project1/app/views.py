f8a59833c870d859894e80f7365ebe0f938abf2b
Daniela Simões <danielasimoes@ua.pt>
2017-10-03 11:12:18 +0100

    struct django added.

---
+++ project1/app/views.py
@@ -0,0 +1,56 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+
+def sendinfo(request):
+    assert isinstance(request, HttpRequest)
+    if 'height' in request.POST and 'weight' in request.POST:
+        height = request.POST['height']
+        weight = request.POST['weight']
+
+        if weight and height:
+            return render(request, 'app/index.html', {'result': (int(height)/int(weight))**2})
---
+++ project1/webproj/urls.py
@@ -0,0 +1,48 @@
+"""webproj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from datetime import datetime
+from django.conf.urls import url
+from django.contrib import admin
+import django.contrib.auth.views
+
+import app.forms
+import app.views
+
+urlpatterns = [
+url(r'^$', app.views.home, name='home'),
+    url(r'^contact$', app.views.contact, name='contact'),
+    url(r'^about', app.views.about, name='about'),
+    url(r'^login/$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': app.forms.BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title': 'Log in',
+                'year': datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^sendinfo', app.views.sendinfo, name='sendinfo'),
+]

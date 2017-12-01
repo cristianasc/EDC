a0c9493412cf38f5365cba74e3a8505c0751052b
Daniela Simões <danielasimoes@ua.pt>
2017-12-01 21:50:59 +0000

    database connected.

--- project2/app/models.py
+++ project2/app/models.py
@@ -1,5 +1,10 @@
+from s4api.graphdb_api import GraphDBApi
+from s4api.swagger import ApiClient
+
 
 class Database:
     def __init__(self):
-        #self.session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
-        pass
+        endpoint = "http://localhost:7200"
+        repo_name = "Spotify"
+        client = ApiClient(endpoint=endpoint)
+        accessor = GraphDBApi(client)

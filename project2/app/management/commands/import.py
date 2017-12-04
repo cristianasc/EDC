46e54d9d805285d1388431610af1f707bd188ea3
Daniela Simões <danielasimoes@ua.pt>
2017-12-04 22:52:16 +0000

    test2

--- project2/app/management/commands/import.py
+++ project2/app/management/commands/import.py
@@ -28,24 +28,13 @@ class Command(BaseCommand):
         file = open("new-releases.rdf", "w")
         file.write(content)
 
-        #db.accessor.upload_data_file("new-releases.rdf", repo_name=db.repo_name)
+        db.accessor.upload_data_file("new-releases.rdf", repo_name=db.repo_name)
 
-        update = """INSERT DATA {
-        <http://dbpedia.org/resource/University_of_Leipzig>
-        <http://dbpedia.org/property/students>
-        "12345678"
+        query = """
+        SELECT ?s ?p ?o
+        WHERE {
+            ?s ?p ?o .
         }"""
-
-        query = {"update": update}
-        db.accessor.sparql_update(body=query, repo_name=db.repo_name)
-
-        query = """PREFIX fb: <http://rdf.freebase.com/ns/>
-                PREFIX dbpedia: <http://dbpedia.org/resource/>
-
-                SELECT ?s ?p ?o
-                WHERE {
-                    ?s ?p ?o .
-                }"""
 
         payload_query = {"query": query}
         print(db.accessor.sparql_select(body=payload_query, repo_name=db.repo_name))
